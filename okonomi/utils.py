f9119ade06f3aaca6c9e3ea4f93d357a72a96446
Nate Smith <nate.smith@coxinc.com>
2011-11-07 14:19:30 -0500

    first pass at generate_js, it's still pretty stubbed

--- okonomi/utils.py
+++ okonomi/utils.py
@@ -1,3 +1,7 @@
+import os
+
+from django.conf import settings
+
 # ([str]|str) -> str
 def make_cache_key(paths):
     """given a list of javascript media paths,
@@ -16,13 +20,22 @@ def make_combined_path(paths):
     generate a string suitable for use in a url that
     combines the paths
     """
-    # TODO hash this
+    # TODO hash this using a key in settings
     return paths.join('|')
 
-# [str] -> str
+# ([str]|str) -> str
 def generate_js(paths):
     """given a list of javascript media paths, read
     them from disk and return their concatenation
     """
-    # TODO write this
-    return ''
+    if type(paths) == type(''):
+        # TODO decrypt
+        paths = paths.split('|')
+
+    combined = ''
+    for path in paths:
+        # TODO use actual setting
+        full_path = os.path.join(settings.STATIC_PATH, path)
+        combined += '\n\n' # TODO slurp file
+
+    return combined

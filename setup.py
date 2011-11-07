a0f98b25ff2c20f362a909da7eb7264d15d3b419
Nate Smith <nate.smith@coxinc.com>
2011-11-07 13:34:52 -0500

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "okonomi",
+    version = "0.1",
+    packages = ['okonomi',],
+    author = "Cox Media Group",
+    author_email = "opensource@coxinc.com",
+    description = "smart JS loading for Django",
+    license = "MIT",
+    url = "https://github.com/coxmediagroup/okonomi",
+)
+
